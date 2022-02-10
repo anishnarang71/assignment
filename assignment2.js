5d437b14d12981b92db1b9ce9778d8eddd2b40f5
anishnarang71 <76154687+anishnarang71@users.noreply.github.com>
2022-02-10 20:39:39 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assignment 2</title>
+</head>
+<body>
+    <script>
+        //to store marks of 5 subjects and calculate percentage
+        var english=83//store marks of subjects
+        var mathematics=76//store marks of subjects
+        var social_science=54//store marks of subjects
+        var hindi=70//store marks of subjects
+        var science=45//store marks of subjects
+        var sum=english+mathematics+social_science+hindi+science
+        var percentage= (sum/500)*100
+        console.log("percentage is:")
+        console.log(percentage)//dispay percentage of marks
+
+
+
+    </script>
+    
+</body>
+</html>
